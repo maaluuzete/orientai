5104e9e6341c3d15c393bcd2415400f5b52724d6
marcosvinicio10 <marcosouzavini2018@gmail.com>
2025-11-02 19:54:22 -0300

    Pegueno Ajuste

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -12,16 +12,16 @@
 <script>
 import Header from './components/Header.vue'
 import Home from './components/Home.vue'
-import Form from './components/Form.vue'
-import Results from './components/Results.vue'
+//import Form from './components/Form.vue'
+//import Results from './components/Results.vue'
 
 export default {
   name: 'App',
   components: {
     Header,
-    Home,
-    Form,
-    Results
+    Home
+    //Form,
+    //Results
   },
   data() {
     return {
